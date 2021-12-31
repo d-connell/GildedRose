ede0a045a6ffd026469b779afa007a572a9eae2a
d-connell <lapetitegecko@gmail.com>
2021-12-31 18:56:08 +1100

    Tidied logic for all other items.

--- src/main/gildedrose/GildedRose.java
+++ src/main/gildedrose/GildedRose.java
@@ -34,17 +34,9 @@ public class GildedRose {
                 return;
             }
 
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-
             item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn < 0) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            }
+            item.quality += item.sellIn < 0 ? -2 : -1;
+            item.quality = Math.max(0, item.quality);
         }
     }
 }
