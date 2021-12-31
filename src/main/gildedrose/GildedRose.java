2e755375654cdeaba1fb1f160a164ed867cc7a6e
d-connell <lapetitegecko@gmail.com>
2021-12-31 19:09:56 +1100

    Moved logic for updating quality to the enums.

--- src/main/gildedrose/GildedRose.java
+++ src/main/gildedrose/GildedRose.java
@@ -16,26 +16,17 @@ public class GildedRose {
 
             item.sellIn = item.sellIn - 1;
 
-            if (item.name.equals(ItemType.PASS.name) && item.sellIn < 0) {
-                item.quality = 0;
-                return;
-            }
-
-            int change;
-
             if (item.name.equals(ItemType.CHEESE.name)) {
-                change= item.sellIn < 0 ? 2 : 1;
+                ItemType.CHEESE.updateQuality(item);
             } else if (item.name.equals(ItemType.PASS.name)) {
-                change = item.sellIn < 5 ? 3
-                        : item.sellIn < 10 ? 2
-                        : 1;
+                ItemType.PASS.updateQuality(item);
             } else {
-                change = item.sellIn < 0 ? -2 : -1;
+                ItemType.NORMAL.updateQuality(item);
             }
 
             int MINIMUM = 0;
             int MAXIMUM = 50;
-            item.quality = Math.max(MINIMUM, item.quality + change);
+            item.quality = Math.max(MINIMUM, item.quality);
             item.quality = Math.min(MAXIMUM, item.quality);
         }
     }
--- src/main/gildedrose/ItemType.java
+++ src/main/gildedrose/ItemType.java
@@ -2,11 +2,35 @@ package main.gildedrose;
 
 public enum ItemType {
 
-    HAND ("Sulfuras, Hand of Ragnaros"),
-    PASS ("Backstage passes to a TAFKAL80ETC concert"),
-    CHEESE("Aged Brie");
+    HAND ("Sulfuras, Hand of Ragnaros") {
+        public void updateQuality(Item item) {
+            // This item never changes.
+        }
+    },
+    PASS ("Backstage passes to a TAFKAL80ETC concert") {
+        public void updateQuality(Item item) {
+            if (item.sellIn < 0) {
+                item.quality = 0;
+            } else {
+                item.quality += item.sellIn < 5 ? 3
+                        : item.sellIn < 10 ? 2
+                        : 1;
+            }
+        }
+    },
+    CHEESE("Aged Brie") {
+        public void updateQuality(Item item) {
+            item.quality += item.sellIn < 0 ? 2 : 1;
+        }
+    },
+    NORMAL("Normal") {
+        public void updateQuality(Item item) {
+            item.quality += item.sellIn < 0 ? -2 : -1;
+        }
+    };
 
-    public String name;
+    public final String name;
+    public abstract void updateQuality(Item item);
 
     ItemType (String name) {
         this.name = name;
