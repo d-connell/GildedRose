79ec7e8fa7e64e87f97bbb7263a7dc2f14e0dbb8
d-connell <lapetitegecko@gmail.com>
2021-12-31 18:54:47 +1100

    Isolated and simplified logic for Sulfuras.

--- src/main/gildedrose/GildedRose.java
+++ src/main/gildedrose/GildedRose.java
@@ -30,21 +30,19 @@ public class GildedRose {
                 return;
             }
 
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                return;
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
+
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+                    item.quality = item.quality - 1;
                 }
             }
         }
