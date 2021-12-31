a2e24b5a3c332447d7aeb002125ff46d0171e6ef
d-connell <lapetitegecko@gmail.com>
2021-12-31 18:57:05 +1100

    Rearranged code to remove duplicate lines for updating item.sellIn

--- src/main/gildedrose/GildedRose.java
+++ src/main/gildedrose/GildedRose.java
@@ -10,15 +10,19 @@ public class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
 
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                return;
+            }
+
+            item.sellIn = item.sellIn - 1;
+
             if (item.name.equals("Aged Brie")) {
-                item.sellIn -= 1;
                 item.quality += item.sellIn < 0 ? 2 : 1;
                 item.quality = Math.min(50, item.quality);
                 return;
             }
 
             if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.sellIn -= 1;
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 } else {
@@ -30,11 +34,6 @@ public class GildedRose {
                 return;
             }
 
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                return;
-            }
-
-            item.sellIn = item.sellIn - 1;
             item.quality += item.sellIn < 0 ? -2 : -1;
             item.quality = Math.max(0, item.quality);
         }
